fd0ba5f81d53c623ccfb8eb50005b98482530c4a
Maja Bojarska <majabojarska98@gmail.com>
2024-03-18 21:15:05 +0100

    Mv, concurrent

--- update_pkg_deps.py
+++ update_pkg_deps.py
@@ -1,21 +1,21 @@
 """
 """
 
-import sys
 import requests
 from dataclasses import dataclass
 import argparse
 from pathlib import Path
 import json
 from typing import Mapping, Sequence, Self
-
+import concurrent.futures
 from copy import deepcopy
 import logging
 
 
 logger = logging.getLogger(__name__)
 
-URL_API_EXP = "https://thunderstore.io/api/experimental"
+_URL_API_EXP = "https://thunderstore.io/api/experimental"
+_DEFAULT_MAX_WORKERS = 5  # Conservative value to avoid rate limiting
 
 
 @dataclass
@@ -39,8 +39,11 @@ class Package:
         return f"{self.namespace}-{self.name}-{self.version}"
 
     def get_latest(self) -> Self:
-        url = f"{URL_API_EXP}/package/{self.namespace}/{self.name}/"
-        latest = requests.get(url).json()["latest"]
+        url = f"{_URL_API_EXP}/package/{self.namespace}/{self.name}/"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        latest = response.json()["latest"]
 
         pkg = Package(
             namespace=self.namespace, name=self.name, version=latest["version_number"]
@@ -67,12 +70,27 @@ def get_arg_parser() -> argparse.ArgumentParser:
     return parser
 
 
-def update_manifest_deps(manifest: Mapping) -> Mapping:
+def update_manifest_deps(
+    manifest: Mapping, max_workers: int = _DEFAULT_MAX_WORKERS
+) -> Mapping:
     manifest = deepcopy(manifest)
     deps_orig: Sequence[Package] = [
         Package.from_str(elem) for elem in manifest["dependencies"]
     ]
-    manifest["dependencies"] = [str(dep.get_latest()) for dep in deps_orig]
+
+    deps_latest = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_pkg = {executor.submit(pkg.get_latest): pkg for pkg in deps_orig}
+        for future in concurrent.futures.as_completed(future_to_pkg):
+            pkg = future_to_pkg[future]
+            try:
+                deps_latest.append(future.result())
+            except Exception as err:
+                raise ValueError(
+                    f"Failed to get latest version for package {pkg=}"
+                ) from err
+
+    manifest["dependencies"] = [str(pkg) for pkg in deps_latest]
 
     return manifest
 
